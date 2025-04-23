715d483ecb0586e5919f52f4236bdd0f70741447
SerJo <60974487+SerJo2@users.noreply.github.com>
2025-04-23 18:17:07 +1000

    updated plugins.json

--- www/js/plugins.js
+++ www/js/plugins.js
@@ -20,7 +20,6 @@ var $plugins =
 {"name":"AudioStreaming","status":true,"description":"Load audio faster and use only ogg files.","parameters":{"mode":"10","deleteM4a":"false"}},
 {"name":"stbvorbis_stream","status":false,"description":"","parameters":{}},
 {"name":"stbvorbis_stream_asm","status":false,"description":"","parameters":{}},
-{"name":"ARP_TitleCommandExit","status":true,"description":"v1.00 Adds an option in the title command window to close\r\ngame window.","parameters":{"Command Exit":"Quit Game","Show Exit":"true"}},
 {
   "name": "LiveSplit",
   "status": true,
